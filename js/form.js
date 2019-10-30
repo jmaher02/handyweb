c389d8a1348d2b1b92b0977810e5dffae2e247ea
jmaher02 <57075950+jmaher02@users.noreply.github.com>
2019-10-29 20:50:14 -0400

    Add service page, work with form submit

---
+++ js/form.js
@@ -0,0 +1,11 @@
+function sendQuote() {
+    document.getElementById("confirm").style.display = "block";
+    document.getElementById('name').style.display='none';
+    document.getElementById('phone').style.display='none';
+    document.getElementById('email').style.display='none';
+    document.getElementById('message').style.display='none';
+    document.getElementById('hint').style.display='none';
+    document.getElementById('attach').style.display='none';
+    document.getElementById('quote').style.display='none';
+    //document.getElementById("name").value = "";
+}
