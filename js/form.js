1ca4c47b015607c83c1a8fedfa00be5b779fae3f
jmaher02 <57075950+jmaher02@users.noreply.github.com>
2019-10-29 23:20:04 -0400

    Just some fixes

--- js/form.js
+++ js/form.js
@@ -4,8 +4,9 @@ function sendQuote() {
     document.getElementById('phone').style.display='none';
     document.getElementById('email').style.display='none';
     document.getElementById('message').style.display='none';
-    document.getElementById('hint').style.display='none';
-    document.getElementById('attach').style.display='none';
+    //For attachment
+    //document.getElementById('hint').style.display='none';
+    //document.getElementById('attach').style.display='none';
     document.getElementById('quote').style.display='none';
     //document.getElementById("name").value = "";
 }
